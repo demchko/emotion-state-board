90d063c75d455f4fa11c8d0f9e50ed3bf0e62e30
demchkoa <demchkoartem05@gmail.com>
2025-06-27 17:28:04 +0300

    added complete finished modal for adding emotion

---
+++ src/types/emotions.ts
@@ -0,0 +1,16 @@
+export type EmotionType = 'joy' | 'sadness' | 'anger' | 'fear' | 'disgust' | 'surprise';
+
+export interface Emotion {
+    id: string;
+    comment: string;
+    type: EmotionType;
+}
+
+export interface EmotionData {
+    icon: string;
+    color: string;
+    borderColor: string;
+    activeStyles: string;
+    name: string;
+}
+export type EmotionsData = Record<EmotionType, EmotionData>;
